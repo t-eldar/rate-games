4d0e928ebffa397a66b06f4c4fd1ba84c869d879
t-eldar <74774738+t-eldar@users.noreply.github.com>
2023-04-19 18:25:38 +0300

    Added seed data to Ratings table

--- backend/RateGames/src/RateGames/DatabaseContext/ApplicationContext.cs
+++ backend/RateGames/src/RateGames/DatabaseContext/ApplicationContext.cs
@@ -9,9 +9,28 @@ public class ApplicationContext : IdentityDbContext<User>, IApplicationContext
 {
 	public DbSet<Rating> Ratings { get; set; }
 	public DbSet<Review> Reviews { get; set; }
+
 	public ApplicationContext(DbContextOptions<ApplicationContext> options)
-		: base(options) 
+		: base(options)
 	{
 		Database.EnsureCreated();
+	}
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.OnModelCreating(builder);
+
+		builder
+			.Entity<Rating>()
+			.HasData(
+				new Rating[]
+				{
+					new Rating { Id = 1, Value = 1 },
+					new Rating { Id = 2, Value = 2 },
+					new Rating { Id = 3, Value = 3 },
+					new Rating { Id = 4, Value = 4 },
+					new Rating { Id = 5, Value = 5 },
+				}
+			);
 	}
 }
