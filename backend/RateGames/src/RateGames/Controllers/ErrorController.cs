05b211e1b65c1149ae3970ec6994dbb5de7d7511
t-eldar <74774738+t-eldar@users.noreply.github.com>
2023-04-19 18:25:20 +0300

    Added error controller

---
+++ backend/RateGames/src/RateGames/Controllers/ErrorController.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateGames.Controllers;
+
+/// <summary>
+/// Error handling controller
+/// </summary>
+[ApiController]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorController : ControllerBase
+{
+	[Route("error")]
+	[AllowAnonymous]
+	public IActionResult HandleError()
+	{
+		var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+		if (exceptionHandlerFeature is null)
+		{
+			return Problem();
+		}
+
+		var exception = exceptionHandlerFeature.Error;
+
+		var result = exception switch
+		{
+			ValidationException => ValidationProblem(),
+			_ => Problem(),
+		};
+
+		return result;
+	}
+
+	[Route("error-development")]
+	public IActionResult HandleErrorDevelopment()
+	{
+		var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+		if (exceptionHandlerFeature is null)
+		{
+			return Problem();
+		}
+
+		return Problem(
+			detail: exceptionHandlerFeature.Error.StackTrace,
+			title: exceptionHandlerFeature.Error.Message
+		);
+	}
+}
