a14f2281f5bad2343bb4416823a06890327a0859
t-eldar <74774738+t-eldar@users.noreply.github.com>
2023-04-22 18:09:00 +0300

    Added extension for id and array comparing

---
+++ backend/RateGames/src/RateGames.Common/Extensions/EntityMarkerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+
+using RateGames.Common.Contracts;
+
+namespace RateGames.Common.Extensions;
+
+public static class EntityMarkerExtensions
+{
+	/// <summary>
+	/// Use for including all properties of database model in query.
+	/// </summary>
+	/// <typeparam name="T">Entity type</typeparam>
+	/// <param name="entity"></param>
+	/// <returns>Unchanged entity</returns>
+	public static T IncludeAllProperties<T>(this T entity)
+		where T : IEntity => entity;
+
+	/// <summary>
+	/// Use for including selected properties in query.
+	/// </summary>
+	/// <typeparam name="T">Entity type</typeparam>
+	/// <param name="entity"></param>
+	/// <returns>Unchanged entity</returns>
+	public static IEnumerable<TEntity> IncludeProperty<TEntity, TProp>(
+		this IEnumerable<TEntity> source,
+		Expression<Func<TEntity, TProp>> selector
+	)
+		where TEntity : IEntity => source;
+
+	/// <summary>
+	/// Use for making filter queries. Instead of writing Id == values.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="id"></param>
+	/// <param name="values"></param>
+	/// <returns></returns>
+	public static bool ContainsAny<T>(this int id, IEnumerable<int> values)
+		where T : IEntity => values.Contains(id);
+}
--- backend/RateGames/src/RateGames.Common/Extensions/EnumerableExtensions.cs
+++ backend/RateGames/src/RateGames.Common/Extensions/EnumerableExtensions.cs
@@ -6,81 +6,81 @@ namespace RateGames.Common.Extensions;
 
 public static class EnumerableExtensions
 {
-    /// <summary>
-    /// Checks if any of passed values are contained in source enumerable.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="values"></param>
-    /// <returns></returns>
-    public static bool ContainsAny<T>(this IEnumerable<T> source, IEnumerable<T> values)
-    {
-        foreach (var value in values)
-        {
-            if (source.Contains(value))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    /// <summary>
-    /// Checks if elements contain any value with passed ids.
-    /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="valueIds"></param>
-    /// <returns></returns>
-    public static bool ContainsAny<TEntity>(this IEnumerable<TEntity> source, IEnumerable<int> valueIds) 
-        where TEntity : IEntity
-    {
-        foreach (var id in valueIds)
-        {
-            if (source.Any(entity => entity.Id == id))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+	/// <summary>
+	/// Checks if any of passed values are contained in source enumerable.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="values"></param>
+	/// <returns></returns>
+	public static bool ContainsAny<T>(this IEnumerable<T> source, IEnumerable<T> values)
+	{
+		foreach (var value in values)
+		{
+			if (source.Contains(value))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	/// <summary>
+	/// Checks if elements contain any value with passed ids.
+	/// </summary>
+	/// <typeparam name="TEntity"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="valueIds"></param>
+	/// <returns></returns>
+	public static bool ContainsAny<TEntity>(this IEnumerable<TEntity> source, IEnumerable<int> valueIds)
+		where TEntity : IEntity
+	{
+		foreach (var id in valueIds)
+		{
+			if (source.Any(entity => entity.Id == id))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /// <summary>
-    /// Checks if all of passed values are contained in source enumerable.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="values"></param>
-    /// <returns></returns>
-    public static bool ContainsAll<T>(this IEnumerable<T> source, IEnumerable<T> values)
-    {
-        foreach (var value in values)
-        {
-            if (!source.Contains(value))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+	/// <summary>
+	/// Checks if all of passed values are contained in source enumerable.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="values"></param>
+	/// <returns></returns>
+	public static bool ContainsAll<T>(this IEnumerable<T> source, IEnumerable<T> values)
+	{
+		foreach (var value in values)
+		{
+			if (!source.Contains(value))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
-    /// <summary>
-    /// Checks if elements contain all values with passed ids.
-    /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="valueIds"></param>
-    /// <returns></returns>
-    public static bool ContainsAll<TEntity>(this IEnumerable<TEntity> source, IEnumerable<int> valueIds)
-        where TEntity : IEntity
-    {
-        foreach (var id in valueIds)
-        {
-            if (!source.Any(entity => entity.Id == id))
-            {
-                return false;
-            }
-        }
+	/// <summary>
+	/// Checks if elements contain all values with passed ids.
+	/// </summary>
+	/// <typeparam name="TEntity"></typeparam>
+	/// <param name="source"></param>
+	/// <param name="valueIds"></param>
+	/// <returns></returns>
+	public static bool ContainsAll<TEntity>(this IEnumerable<TEntity> source, IEnumerable<int> valueIds)
+		where TEntity : IEntity
+	{
+		foreach (var id in valueIds)
+		{
+			if (!source.Any(entity => entity.Id == id))
+			{
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
