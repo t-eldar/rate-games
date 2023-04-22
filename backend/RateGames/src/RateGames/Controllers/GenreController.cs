607a7112bc4fc563f7983e6c02b10787f1a438c9
t-eldar <74774738+t-eldar@users.noreply.github.com>
2023-04-22 10:02:12 +0300

    Added genre controller

---
+++ backend/RateGames/src/RateGames/Controllers/GenreController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using RateGames.Services.Interfaces;
+
+namespace RateGames.Controllers;
+
+/// <summary>
+/// Controller for getting genres from <see href="igdb.com"/>
+/// </summary>
+[Route("genres")]
+[ApiController]
+public class GenreController : ControllerBase
+{
+	private readonly IGenreService _genreService;
+	public GenreController(IGenreService genreService) => _genreService = genreService;
+
+	[Route("{id}")]
+	[HttpGet]
+	public async Task<IActionResult> GetByIdAsync(int id)
+	{
+		var genre = await _genreService.GetByIdAsync(id);
+
+		return genre is null
+			? NotFound() 
+			: Ok(genre);
+	}
+}
