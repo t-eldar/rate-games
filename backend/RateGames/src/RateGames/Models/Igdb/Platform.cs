833ca5603c1fde6fbad660ad5bec3789d4b9b5a3
t-eldar <74774738+t-eldar@users.noreply.github.com>
2023-04-26 20:35:00 +0300

    Fixed platform model

--- backend/RateGames/src/RateGames/Models/Igdb/Platform.cs
+++ backend/RateGames/src/RateGames/Models/Igdb/Platform.cs
@@ -1,5 +1,6 @@
 ﻿using RateGames.Common.Contracts;
 using RateGames.Common.Utils;
+using RateGames.Models.Igdb.Enums;
 
 namespace RateGames.Models.Igdb;
 
@@ -9,6 +10,7 @@ public class Platform : IEntity
     public string? Abbreviation { get; set; }
     public string? Name { get; set; }
     public int? Generation { get; set; }
+    public PlatformCategory? Category { get; set; }
     public string? Summary { get; set; }
     public IdOr<Image>? PlatformLogo { get; set; }
 }
