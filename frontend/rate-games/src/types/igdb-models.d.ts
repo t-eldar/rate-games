32f728b3d58bf9c7e2f87b6659b6c4fe976160f8
t-eldar <74774738+t-eldar@users.noreply.github.com>
2023-04-26 20:51:20 +0300

    Deleted response models

--- frontend/rate-games/src/types/igdb-models.d.ts
+++ frontend/rate-games/src/types/igdb-models.d.ts
@@ -1,81 +1,73 @@
 export type Game = {
   id: number;
-  name: string;
-  summary: string;
-  firstReleaseDate: number;
-  aggregatedRating: number;
-  rating: number;
-  category: GameCategory;
-  gameModes: (number | GameModeResponse)[];
-  screenshots: (number | ImageResponse)[];
-  involvedCompanies: (number | InvolvedCompanyResponse)[];
-  genres: (number | GenreResponse)[];
-  gameEngines: (number | GameEngineResponse)[];
-  similarGames: (number | GameResponse)[];
-  platforms: (number | PlatformResponse)[];
+  name?: string;
+  summary?: string;
+  firstReleaseDate?: number;
+  aggregatedRating?: number;
+  rating?: number;
+  category?: GameCategory;
+  gameModes?: (number | GameMode)[];
+  screenshots?: (number | Image)[];
+  involvedCompanies?: (number | InvolvedCompany)[];
+  genres?: (number | Genre)[];
+  gameEngines?: (number | GameEngine)[];
+  similarGames?: (number | Game)[];
+  platforms?: (number | Platform)[];
 };
-export type GameResponse = Partial<Game>;
 
 export type Company = {
   id: number;
-  name: string;
-  country: number;
-  startDate: number;
-  logo: number | ImageResponse;
-  published: (number | GameResponse)[];
-  developed: (number | GameResponse)[];
+  name?: string;
+  country?: number;
+  startDate?: number;
+  logo?: number | Image;
+  published?: (number | Game)[];
+  developed?: (number | Game)[];
 };
-export type CompanyResponse = Partial<Company>;
 
 export type GameEngine = {
   id: number;
-  name: string;
-  description: string;
-  logo: number | ImageResponse;
+  name?: string;
+  description?: string;
+  logo?: number | Image;
 };
-export type GameEngineResponse = Partial<GameEngine>;
 
 export type GameMode = {
   id: number;
-  name: string;
+  name?: string;
 };
-export type GameModeResponse = Partial<GameMode>;
 
 export type Genre = {
   id: number;
-  name: string;
+  name?: string;
 };
-export type GenreResponse = Partial<Genre>;
 
 export type Image = {
   id: number;
-  width: number;
-  height: number;
-  url: string;
-  imageId: string;
+  width?: number;
+  height?: number;
+  url?: string;
+  imageId?: string;
 };
-export type ImageResponse = Partial<Image>;
 
 export type InvolvedCompany = {
   id: number;
-  company: number | CompanyResponse;
-  developer: boolean;
-  porting: boolean;
-  publisher: boolean;
-  supporting: boolean;
+  company?: number | Company;
+  developer?: boolean;
+  porting?: boolean;
+  publisher?: boolean;
+  supporting?: boolean;
 };
-export type InvolvedCompanyResponse = Partial<InvolvedCompany>;
 
 export type Platform = {
   id: number;
-  abbreviation: string;
-  name: string;
-  generation: number;
-  category: PlatformCategory;
-  summary: string;
-  platformLogo: number | ImageResponse;
+  abbreviation?: string;
+  name?: string;
+  generation?: number;
+  category?: PlatformCategory;
+  summary?: string;
+  platformLogo?: number | Image;
 };
-export type PlatformResponse = Partial<Platform>;
 
 enum GameCategory {
   MainGame = 0,
